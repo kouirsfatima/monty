adf83c6f3890f2155e20e68ff7301999e3a13e28
kouirsfatima <fatimaberline535@gmail.com>
2023-09-19 17:26:00 +0100

    modified main.c

--- main.c
+++ main.c
@@ -1,39 +1,43 @@
-#include"monty.h"
-/**
- * main - Entry points
- * Description: create an interpreter for Monty ByteCodes files.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
-*/
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
     FILE *file_fb;
     int i = 0;
     char line_number[1024];
+    char *token;
+    int j = 0; 
 
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
-       exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
     file_fb = fopen(argv[1], "r");
 
     if (file_fb == NULL)
     {
-       fprintf(stderr, "Error: Can't open file \n");
-		exit(EXIT_FAILURE);
-	}
-        
-      while (fgets(line_number, sizeof(line_number), file_fb) != NULL)
+        fprintf(stderr, "Error: Can't open file \n");
+        exit(EXIT_FAILURE);
+    }
 
+    while (fgets(line_number, sizeof(line_number), file_fb) != NULL)
     {
         i++;
+        /**printf("%d%s", i, line_number);*/
+        token = strtok(line_number, "  \n");
+        while (token != NULL)
         {
-            printf("%d%s\n", i, line_number);
+            printf("%s\n", token);
+            token = strtok(NULL, "  \n");
+            j++; 
         }
     }
+
     fclose(file_fb);
     return 0;
 }
